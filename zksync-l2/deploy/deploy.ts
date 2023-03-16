ea8e3cbd325cb0701347c3d53152fde53056cabe
Dmitry Stryukovsky <strukovsky1@gmail.com>
2023-03-16 19:02:36 +0400

    feat: zksync contract

---
+++ zksync-l2/deploy/deploy.ts
@@ -0,0 +1,31 @@
+import { Wallet, utils } from "zksync-web3";
+import * as ethers from "ethers";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
+import {config} from "dotenv";
+config();
+
+const PRIVATE_KEY: string = process.env.PRIVATE_KEY || "";
+if (!PRIVATE_KEY) {
+  throw new Error("Please set PRIVATE_KEY in the environment variables.");
+}
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+  const contractName = "ZksyncForest";
+  console.log(`Running deploy script for the ${contractName} contract`);
+  const wallet = new Wallet(PRIVATE_KEY);
+  const deployer = new Deployer(hre, wallet);
+  const artifact = await deployer.loadArtifact(contractName);
+  const deploymentFee = await deployer.estimateDeployFee(artifact, []);
+  const depositHandle = await deployer.zkWallet.deposit({
+    to: deployer.zkWallet.address,
+    token: utils.ETH_ADDRESS,
+    amount: deploymentFee.mul(2),
+  });
+  await depositHandle.wait();
+  const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
+  console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
+  const greeterContract = await deployer.deploy(artifact, []);
+  const contractAddress = greeterContract.address;
+  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+}
