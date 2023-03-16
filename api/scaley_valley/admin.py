a90f23a75a4096c40543dec5d654008c88f52dce
Dmitry <d.suldin@ongrid.pro>
2023-03-16 15:44:08 +0400

    feat: add base objects models

--- api/scaley_valley/admin.py
+++ api/scaley_valley/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Resource, Chain, Kind, Valley, Character
 
-# Register your models here.
+
+@admin.register(Resource)
+class ResourceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Chain)
+class ChainAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Kind)
+class KindAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Valley)
+class ValleyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    list_display = ('contract_token_id', 'owner',
+                    'price', 'valley', 'creation_time', 'last_update')
+    search_fields = ('kind__name', 'contract_token_id', 'owner', 'valley__name')
