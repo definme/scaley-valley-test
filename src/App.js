e0d5dfdd55875f945074ea2c5632665b99ebfae7
coldwithu <dima061101@gmail.com>
2023-03-16 11:59:02 +0300

    feat: add material ui and header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import * as React from 'react'
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography'
+import Box from '@mui/material/Box'
+import Header from './components/Header'
+
 function App() {
-  return <div>Scaley Valley app</div>
+  return (
+    <>
+      <Header />
+      <Container>
+        <Box sx={{ my: 4 }}>
+          <Typography variant='h4' component='h1' gutterBottom>
+            Scaley Valley app
+          </Typography>
+        </Box>
+      </Container>
+    </>
+  )
 }
 
 export default App
