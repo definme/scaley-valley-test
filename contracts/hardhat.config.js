5b966eab1e7bbf71691174613596b6ca8d4d08f9
Dmitry <d.suldin@ongrid.pro>
2023-03-17 16:26:38 +0400

    chore: deploy script for collection contract

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -1,6 +1,37 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox')
+require('hardhat-deploy')
+require('hardhat-deploy-ethers')
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const accounts = {
+  mnemonic: `${process.env.MNEMONIC}`,
+}
+
 module.exports = {
-  solidity: "0.8.18",
-};
+  solidity: '0.8.18',
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+  },
+  networks: {
+    localhost: {
+      url: 'http://localhost:8545/',
+      accounts,
+      live: false,
+      saveDeployments: true,
+      tags: ['local'],
+    },
+    hardhat: {
+      accounts,
+    },
+    goerli: {
+      chainId: 5,
+      url: 'https://endpoints.omniatech.io/v1/eth/goerli/public',
+      accounts,
+      live: true,
+      saveDeployments: true,
+    },
+  },
+}
