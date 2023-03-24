d0f4924f854b187a532f5e12fc99de3d86a7819f
coldwithu <dima061101@gmail.com>
2023-03-24 17:29:32 +0300

    feat: change valleys styles

--- app/src/pages/ExploreValleys/index.jsx
+++ app/src/pages/ExploreValleys/index.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 import Container from '@mui/material/Container'
 import Valley from '../../components/Valley'
 import { getValleys } from '../../api'
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 function ExploreValleys() {
   const [valleys, setValleys] = useState([])
@@ -19,11 +21,44 @@ function ExploreValleys() {
   }, [])
 
   return (
-    <Container>
-      {valleys.map((valley, idx) => (
-        <Valley key={idx} {...valley} />
-      ))}
-    </Container>
+    <Box>
+        <Typography
+            sx={{
+              fontFamily: "'Inter', sans-serif",
+              fontStyle: 'normal',
+              fontWeight: '600',
+              fontSize: '40px',
+              lineHeight: '48px',
+              textAlign: 'center',
+              letterSpacing: '0.01em',
+              color: 'white',
+              marginTop: '135px',
+            }}
+        >
+            Explore the valleys
+        </Typography>
+        <Typography
+            sx={{
+              fontFamily: "'Inter', sans-serif",
+              fontStyle: 'normal',
+              fontWeight: '500',
+              fontSize: '15px',
+              lineHeight: '18px',
+              textAlign: 'center',
+              letterSpacing: '0.01em',
+              margin: '12px 0 68px 0',
+              color: '#616572',
+            }}
+        >
+            You can choose one to travel to this valley
+        </Typography>
+      <Box>
+        {valleys.map((valley, idx) => (
+            <Valley key={idx} {...valley} />
+        ))}
+      </Box>
+
+    </Box>
   )
 }
 
